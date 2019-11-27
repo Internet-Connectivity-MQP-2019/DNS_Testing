0df37df2bbbd80144c46a2697ee3b705e7d540bb
samgoldman <sgoldman216@gmail.com>
2019-11-27 13:33:15 -0500

    Script updates

--- analysis.sh
+++ analysis.sh
@@ -8,6 +8,7 @@ if [ ! -f results/geolocation.csv ]; then
 fi
 
 COV_MAX="0.05"
+JOB_COUNT=32
 
 echo "Steps 9 & 10"
 echo "recursive_ip" > results/reliable_recursive_ips.csv
@@ -17,5 +18,5 @@ echo "authoritative_ip" > results/reliable_authoritative_ips.csv
 ./parallel -a results/authoritative_reliability_results.csv --colsep , --header '.*\n' --progress --eta --jobs $JOB_COUNT ./filter_coefficient_of_variance.py {1} {2} {3} {4} {5} $COV_MAX >> results/reliable_authoritative_ips.csv
 
 echo "Step 11"
-echo "recursive_state,authoritative_state,min_rtt,max_rtt,mean_rtt,median_rtt" > results/aggregatio_state_to_state.csv
-./aggregate_state_to_state.py /test_results.csv results/reliable_recursive_ips.csv results/reliable_authoritative_ips.csv results/geolocation.csv >> results/aggregatio_state_to_state.csv
+echo "recursive_state,authoritative_state,min_rtt,max_rtt,mean_rtt,median_rtt" > results/aggregation_state_to_state.csv
+./aggregate_state_to_state.py /test_results.csv results/reliable_recursive_ips.csv results/reliable_authoritative_ips.csv results/geolocation.csv >> results/aggregation_state_to_state.csv
